c136da532264af9610a9683685fdebef5e5f92aa
Lennart Oymanns <oymanns@physik.rwth-aachen.de>
2016-06-19 00:38:58 +0200

    Initial Commit

---
+++ vegas/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  vegas
+//
+//  Created by Lennart Oymanns on 19/06/16.
+//  Copyright © 2016 Lennart Oymanns. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
